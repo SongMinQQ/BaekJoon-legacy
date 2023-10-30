5cca9093254433ada87dbcedac1f04b957fa2d5b
SongMinQQ <koe7324@naver.com>
2023-10-30 17:19:22 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ int main() {
 
 	cin >> sentence;
 
-	string alphabet[7] = { "c=", "c-", "d-", "lj", " nj", "s=", "z=" };
+	string alphabet[7] = { "c=", "c-", "d-", "lj", "nj", "s=", "z=" };
 	string alphabet2 = "dz=";
 
 	int count = 0;
@@ -20,7 +20,12 @@ int main() {
 			}
 		}
 	}
-	
-	cout << count;
+	for (int i = 0; i < sentence.size() - 2; i++) {
+		if (sentence[i] == alphabet2[0] && sentence[i + 1] == alphabet2[1] && sentence[i + 2] == alphabet2[2]) {
+			count++;
+		}
+	}
+
+	cout << sentence.length() - count;
 	return 0;
 }
