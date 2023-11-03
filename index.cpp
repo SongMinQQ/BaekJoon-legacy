66db2f73eb80f53519e535c028fee1fe13a5f0da
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-11-03 20:37:09 +0900

    심화1 다품

--- index.cpp
+++ index.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+class Person {
+    int id;
+    double weight;
+    string name;
+public:
+    Person(int id , string name, double weight);
+    void show() {
+        cout << id << ' ' << weight <<' ' << name << endl;
+    }
+};
 
+Person::Person(int id = 1, string name = "Grace", double weight = 20.5){
+    
+}
 
 int main(){
-    
+    Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
+    grace.show();
+    ashley.show();
+    helen.show();
+
 }
---
+++ 심화 1/2941.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string sentence;
+
+	cin >> sentence;
+
+	if (sentence.length() == 1) {
+		cout << 1;
+		return 0;
+	}
+
+	string alphabet[7] = { "c=", "c-", "d-", "lj", "nj", "s=", "z=" };
+	string alphabet2 = "dz=";
+
+	int count = 0;
+	
+	for (int j = 0; j < 7; j++) {
+		for (int i = 0; i < sentence.size() - 1; i++) {
+			if (sentence[i] == alphabet[j][0] && sentence[i + 1] == alphabet[j][1]) {
+				count++;
+			}
+		}
+	}
+	for (int i = 0; i < sentence.size() - 2; i++) {
+		if (sentence[i] == alphabet2[0] && sentence[i + 1] == alphabet2[1] && sentence[i + 2] == alphabet2[2]) {
+			count++;
+		}
+	}
+	
+	cout << sentence.length() - count;
+
+	return 0;
+}
