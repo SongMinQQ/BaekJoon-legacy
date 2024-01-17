fe93d3182560bfa4ee846bd43ef5473bd0e15e1d
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-01-17 20:31:35 +0900

    2024-01-17

--- 이분 탐색/1654.cpp
+++ 이분 탐색/1654.cpp
@@ -1,24 +1,39 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int calculate(int a[], int b, int c){
-    
-}
-int main(){
-    int k, n, count, lanNum, max = 0, result;
+long long k, n;
+vector<long long> v;
 
-    int *lan = new int[k];
-    cin >> k >> n;
-
-    for(int i = 0; i < k; i++){
-        cin >> lanNum;
-        lan[i] = lanNum;
-        max = (lan[i] > max) ? lan[i] : max;
-    }
-    for(int i = 0; i < k; i++){
-        if((lan[i] / max) < 1){
-            max = max * 0.5;
+long long length(long long start, long long end){
+    long long result = 0;
+    while(start <= end){
+        cout << start << " : " << end << '\n';
+        long long mid = (start + end) / 2;
+        long long sum = 0;
+        for(long long num : v){
+            sum += num / mid;
+        }
+        if (sum >= n) {
+            result = mid;
+            start = mid + 1;
+        } else {
+            end = mid - 1;
         }
     }
+    cout << start << " : " << end << '\n';
+
+    return result;
+}
+
+int main(){
+    long long max = 0, num;
+    cin >> k >> n;
+    for(int i = 0; i < k; i++){
+        cin >> num;
+        v.push_back(num);
+        max = num > max ? num : max;
+    }
+    cout << length(1, max);
+
+    return 0;
 }
